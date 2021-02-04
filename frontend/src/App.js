06e7919d6d0789e9104622991eb861d5a05a87dc
alinamiryuk <alinamiryuk@mail.ru>
2021-02-04 22:19:05 +0300

    colors palette

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 
 export default function App() {
-  const [dark, setDark] = useState(false);
+  const [dark, setDark] = useState(false)
 
   return (
-    <div className={`w-full md:h-screen ${dark ? 'dark' : ''}`}>
-      <div className="bg-gray-100 dark:bg-gray-900 dark:text-white">
-        <button onClick={() => setDark(state => !state)}>
-          theme
-        </button>
+    <div className={`min-w-full md:h-screen ${dark ? 'dark' : ''}`}>
+      <div className="container bg-true-gray-400 dark:bg-true-gray-900 min-w-full md:h-screen">
+        <div className="contentWrapper bg-white dark:bg-true-gray-800">
+          <div
+            className="switchWrapper"
+            onClick={() => setDark((state) => !state)}
+          ></div>
+          <div className="progressbarWrapper"></div>
+        </div>
       </div>
     </div>
-  );
+  )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          100: '#f4f4f5',
+        },
+        'true-gray': {
+          400: '#e5e5e5',
+          800: '#262626',
+          900: '#171717',
+        },
+        'cool-gray': {
+          200: '#e5e7eb',
+        },
+        blue: {
+          600: '#2563eb',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
